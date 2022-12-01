67bda904db2447e3c0cf1e04dd3eee81b8f244eb
Bricecrx <94597541+Bricecrx@users.noreply.github.com>
2022-12-01 09:31:10 +0100

    Add of ImagePgm class

---
+++ TP3/src/main/java/brice_crx/tp3/ImagePgm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package brice_crx.tp3;
+
+/**
+ *
+ * @author conra
+ */
+public class ImagePgm {
+    private int width;
+    private int height;
+    private int highestGreyLevel;
+    private int[] i;
+}
