96195a8fdbb0e6c45970da9fad2e5f4a87f2a694
Bricecrx <94597541+Bricecrx@users.noreply.github.com>
2022-12-01 09:40:38 +0100

    Constuctor and getter images

--- TP3/src/main/java/brice_crx/tp3/ImagePgm.java
+++ TP3/src/main/java/brice_crx/tp3/ImagePgm.java
@@ -4,6 +4,9 @@
  */
 package brice_crx.tp3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author conra
@@ -12,5 +15,54 @@ public class ImagePgm {
     private int width;
     private int height;
     private int highestGreyLevel;
-    private int[][] image;
+    private List<List<Integer>> image;
+    
+    public ImagePgm(int w,int h) {
+        width = w;
+        height = h;
+        highestGreyLevel = 255;
+        image = new ArrayList();
+        List<Integer> list;
+        for (int i = 0;i<height;i++) {
+            list = new ArrayList();
+            for (int j=0;j<width;j++) {
+                list.add(0);
+            }
+            image.add(list);
+        }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHighestGreyLevel() {
+        return highestGreyLevel;
+    }
+
+    public void setHighestGreyLevel(int highestGreyLevel) {
+        this.highestGreyLevel = highestGreyLevel;
+    }
+
+    public List<List<Integer>> getImage() {
+        return image;
+    }
+
+    public void setImage(List<List<Integer>> image) {
+        this.image = image;
+    }
+    
+    
 }
